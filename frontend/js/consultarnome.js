ac46484c3299d20b214e7205c62c0594e6fa5e3c
fabiotrevisan05 <fabiojosetrevisan@gmail.com>
2026-08-04 09:19:37 -0300

    Sua mensagem de commit

--- frontend/js/consultarnome.js
+++ frontend/js/consultarnome.js
@@ -4,9 +4,9 @@ let nome = document.getElementById('nome')
 
 btn.addEventListener('click',(e) =>{
 e.preventDefault()
-const nomeUsuario = nome.value.trim();
 
-fetch(`http://localhost:3000/usuario/nome/${encodeURIComponent(nomeUsuario)}`,{
+
+fetch(`http://localhost:3000/usuario/nome/${nome.value}`,{
    
 })
 .then((res)=>res.json())
